c737515c7fdabdb687c0e6bdda6b1f5950e8f13e
nickfarm27 <nicholasfarm27@gmail.com>
2023-01-24 20:44:18 +0800

    Order wallets by id

--- src/services/wallets/getWallets.js
+++ src/services/wallets/getWallets.js
@@ -1,7 +1,10 @@
 import supabase from "../../supabaseClient.js";
 
 export const getWallets = async (req, res) => {
-  const query = supabase.from("wallets").select("*", { count: "exact" });
+  const query = supabase
+    .from("wallets")
+    .select("*", { count: "exact" })
+    .order("id");
 
   const { data, error, count } = await query;
   if (error) {
